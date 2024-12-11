5086e2262619adf7760a4bb725b0030d53f3fd42
stinelock <eaa24stlo@students.eaaa.dk>
2024-12-11 17:20:08 +0100

    Radarpass og vælg vare til pc

--- js/script.js
+++ js/script.js
@@ -7,3 +7,19 @@ navBtn.onclick = () => {
     navBar.classList.toggle('nav-active'); // Toggle active nagivation
     navBtn.classList.toggle('btn-active'); // Toggle active button
 }
+
+
+/* ----------------- Vælg vare--------------------------- */
+document.getElementById('plus').addEventListener('click', function() {
+    var items = document.getElementById('items'); /*definerer items som variabel*/
+    if (items.value < 4) {                          /*kan maks være 4 varer */
+        items.value = parseInt(items.value) + 1;    /*når click bliver antal +1 */
+    }
+});
+
+document.getElementById('minus').addEventListener('click', function() {
+    var items = document.getElementById('items');
+    if (items.value > 1) {
+        items.value = parseInt(items.value) - 1; /*når click bliver antal -1 */
+    }
+});
