9d966977e032bc6c44139003a606d8feb18c9641
stinelock <eaa24stlo@students.eaaa.dk>
2024-12-12 15:03:30 +0100

    Galleri done

--- js/script.js
+++ js/script.js
@@ -10,19 +10,40 @@ navBtn.onclick = () => {
 
 
 /* ----------------- Vælg vare--------------------------- */
-document.getElementById('plus').addEventListener('click', function() {
+document.getElementById('plus').addEventListener('click', function () {
     var items = document.getElementById('items'); /*definerer items som variabel*/
     if (items.value < 4) {                          /*kan maks være 4 varer */
         items.value = parseInt(items.value) + 1;    /*når click bliver antal +1 */
     }
 });
 
-document.getElementById('minus').addEventListener('click', function() {
+document.getElementById('minus').addEventListener('click', function () {
     var items = document.getElementById('items');
     if (items.value > 1) {
         items.value = parseInt(items.value) - 1; /*når click bliver antal -1 */
     }
 });
 
+/*----------------- GÅ TIL BETALING--------------------------- */
+function myFunction() {
+    window.location.href = 'betaling.html?name=';
+}
 
-/**----------------- Betalingsformular--------------------------- */
+/*----------------- Betalingsformular--------------------------- */
+function getQueryParams() {
+    var params = {};
+    var queryString = window.location.search.substring(1);
+    var regex = /([^&=]+)=([^&]*)/g;
+    var match;
+    while (match = regex.exec(queryString)) {
+        params[decodeURIComponent(match[1])] = decodeURIComponent(match[2]);
+    }
+    return params;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var params = getQueryParams();
+    if (document.getElementById('userInfo')) {
+        document.getElementById('userInfo').innerText = 'Navn: ' + params.name + '\nFødselsdato: ' + params.birthdate + '\nEmail: ' + params.email;
+    }
+});
